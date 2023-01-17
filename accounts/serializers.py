e1c3026680b6295ef6a0bd733e6fd4345e8e5723
DESKTOP-0N9Q9LH\a <ingunbi@gmail.com>
2023-01-17 16:52:38 +0900

    Django 스타트 템플릿(api 인증 포함)

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = ['id', 'user_id', 'name', 'email']
+    fields = ['id', 'user_id', 'password', 'name', 'email']   # '__all__'
+    write_only_fields = ('password',)
+
+  def create(self, validated_data):
+    if "password" in validated_data:      
+      validated_data["password"] = make_password(validated_data["password"])
+    return super().create(validated_data)
+
+  def update(self, instance, validated_data):
+    if "password" in validated_data:
+      validated_data["password"] = make_password(validated_data["password"])
+    return super().update(instance, validated_data)
--- api/permissions.py
+++ api/permissions.py
@@ -4,8 +4,24 @@ class IsOwnerOrAdmin(BasePermission):
   def has_object_permission(self, request, view, obj):
     if request.user.is_staff:
       return True
-    return obj.owner == request.user
+    return obj.owner == request.user 
+
+
+class IsSelfOrAdmin(BasePermission):
+  def has_object_permission(self, request, view, obj):
+    if request.user.is_staff:
+      return True
+   
+    return obj == request.user
+
 
 class ReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
+  def has_permission(self, request, view):
+    return request.method in SAFE_METHODS
+
+
+class UserCreateOnly(BasePermission):
+  def has_permission(self, request, view):
+    return request.method == 'POST'
+
+
