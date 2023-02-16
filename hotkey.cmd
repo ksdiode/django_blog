b89a218d6f8c06a7aa34ba046a85ceb8817d9dbc
DESKTOP-0N9Q9LH\a <ingunbi@gmail.com>
2023-02-16 17:13:55 +0900

    버전1

--- hotkey.cmd
+++ hotkey.cmd
@@ -5,4 +5,5 @@ doskey migrate=python manage.py migrate $*
 doskey django-app=python manage.py startapp $*
 doskey commit=git commit $*
 doskey push=git push -u origin main
+doskey pull=git pull origin main
 @echo on
